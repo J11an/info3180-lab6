36c3d9c39158371becb296e51d0cd7af4c8622fc
Jason Wan <jaysun1w@gmail.com>
2022-03-29 18:23:18 -0500

    First part of lab completed

---
+++ src/components/NewsList.vue
@@ -0,0 +1,23 @@
+<template>
+<ul class="class news__list">
+    <li class="news__item">News Item 1</li> 
+    <li class="news__item">News Item 2</li> 
+    <li class="news__item">News Item 3</li>
+</ul>
+</template>
+
+<script>
+
+</script>
+
+export default {
+
+    data() {
+
+        return {};
+    },
+};
+
+<style>
+
+</style>
